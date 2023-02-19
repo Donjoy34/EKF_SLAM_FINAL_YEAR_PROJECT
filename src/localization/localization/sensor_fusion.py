0448a4d366bc5b5ef39a8158c8c73fde63292f6d
Nihad jifri <nihadjifri@gmail.com>
2023-02-19 23:52:49 +0000

    added imu and odom publishers

--- src/localization/localization/sensor_fusion.py
+++ src/localization/localization/sensor_fusion.py
@@ -1,7 +1,9 @@
 import rclpy
 # from rclpy import init, spin, shutdown
 from rclpy.node import Node
-from rclpy.time import Duration, Time
+from rclpy.time import Duration
+from rclpy.time import Time
+from rclpy.clock import ClockType
 from rclpy.qos import qos_profile_sensor_data
 from message_filters import Subscriber, ApproximateTimeSynchronizer
 from numpy import array, eye
@@ -9,6 +11,7 @@ from numpy.linalg import inv
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import TwistWithCovarianceStamped
 from nav_msgs.msg import Odometry
+
 
 class SensorFusion(Node):
 
@@ -26,6 +29,12 @@ class SensorFusion(Node):
 
         # Initialise velocity publisher
         self.publisher = self.create_publisher(Odometry, 'velocity', 1)
+
+
+        self.Odom_publisher = self.create_publisher(Odometry, 'Odom_Velocity', 1)
+        self.Imu_publisher = self.create_publisher(Imu, 'Imu_Velocity', 1)
+
+        
 
         self.previous_time = self.get_clock().now()
 
@@ -59,9 +68,8 @@ class SensorFusion(Node):
         return
 
     def callback(self, wheel_data: Odometry, imu_data: Imu):
-        self.get_logger().info("Imu data : imu_data.linear_acceleration.x ------------------------------------------------>    :")
+        # self.get_logger().info("Imu data : imu_data.linear_acceleration.x ------------------------------------------------>    :")
         # self.get_logger().info(str(imu_data.linear_acceleration.x))
-
 
         # ---------------- Test contents to be removed : From here --------------------->
 
@@ -70,45 +78,31 @@ class SensorFusion(Node):
         new_state=[0.0, 0.0]
         linear_acceleration = imu_data.linear_acceleration.x
 
-        # # Convert Time to TimeMsg
-        # current_time_msg = self.previous_time.to_msg()
+        # Add 2 seconds to previous_time
+        new_time_sec = self.previous_time.nanoseconds // 1e9 + time_delta
+        new_time_nsec = self.previous_time.nanoseconds % 1e9
+        new_time = Time(seconds=new_time_sec, nanoseconds=new_time_nsec, clock_type=ClockType.ROS_TIME)
 
-        # # Add 2 seconds to TimeMsg
-        # current_time_msg.sec += time_delta
+        # self.get_logger().info("previous_time:   :")
+        # self.get_logger().info(str(self.previous_time))
 
-        # # Create a new Time variable with the added seconds
-        # new_time = Time.from_msg(current_time_msg)
+        # self.get_logger().info("new_time:   :")
+        # self.get_logger().info(str(new_time))
 
-        new_time = Time.nanoseconds_to_time(self.previous_time.nanoseconds + 2e9)
+        # self.get_logger().info("current_time:   :")
+        # self.get_logger().info(str(current_time))
 
-
-
-        self.get_logger().info("previous_time:   :")
-        self.get_logger().info(str(self.previous_time))
-
-        self.get_logger().info("new_time:   :")
-        self.get_logger().info(str(new_time))
-
-        self.get_logger().info("current_time:   :")
-        self.get_logger().info(str(current_time))
-
-        # self.get_logger().info("self.state:   :")
-        # self.get_logger().info(str(self.state))
-
-        # if (self.previous_time + (time_delta * (10 ^ 9)) == current_time):
-        if new_time == current_time:
+        if new_time < current_time:
             new_state[0]= self.state[0] + ( linear_acceleration * time_delta )  # Velocity
             new_state[1]= linear_acceleration                                   # Acceleration
 
             self.state = new_state
 
+            self.previous_time = new_time
+
             self.get_logger().info("UPDATED STATE :   :")
             self.get_logger().info(str(self.state))
 
-
-        
-
-        
         # self.get_logger().info("self.previous_time    :")
         # self.get_logger().info(str(self.previous_time))
 
@@ -134,9 +128,10 @@ class SensorFusion(Node):
 
         self.publisher.publish(wheel_data)
 
+        imu_data.linear_acceleration.y = self.state[0]
 
-
-
+        self.Odom_publisher.publish(wheel_data)
+        self.Imu_publisher.publish(imu_data)
 
         # Imu data publisher command :
         # ros2 topic pub /imu sensor_msgs/Imu '{header: {stamp: {sec: 732, nanosec: 55000000}, frame_id: "imu_frame"}, linear_acceleration: {x: 1.0, y: 2.0, z: 3.0}, angular_velocity: {x: 4.0, y: 5.0, z: 6.0}}'
