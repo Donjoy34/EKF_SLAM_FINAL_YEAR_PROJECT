5e609a8dd2f7e9ba2ab9af598e93578e8278f99e
Rephos2 <rb22adc@herts.ac.uk>
2023-03-29 16:26:21 +0000

    Subscription to cameras

--- src/perception/depth_viewer/depth_viewer/depth_viewer.py
+++ src/perception/depth_viewer/depth_viewer/depth_viewer.py
@@ -16,6 +16,35 @@ from eufs_msgs.msg import BoundingBoxes
 class DepthViewer(Node):
     def __init__(self):
         super().__init__('depth_viewer')
+   # Subscribe to the left rectified image
+        self.left_sub_ = message_filters.Subscriber(
+            self,
+            Image,
+            '/camera/left/image_rect_color',
+        )
+
+        # Subscribe to the right rectified image
+        self.right_sub_ = message_filters.Subscriber(
+            self,
+            Image,
+            '/camera/right/image_rect_color',
+        )
+
+        # Create the approximate time synchroniser to sync the subscribers
+        self.ats_ = message_filters.ApproximateTimeSynchronizer(
+            [self.left_sub_, self.right_sub_],
+            100,
+            0.1  # The slop should almost always be small!
+        )
+        # Register the callback with the approximate time synchroniser
+        self.ats_.registerCallback(self.callback)
+
+    def callback(self, left_img_msg, right_img_msg):
+        """
+        Match features between left & right images and calculate a depth map.
+        """
+        print("Received data from cameras successfully.")
+        pass
 
 
 def main():
